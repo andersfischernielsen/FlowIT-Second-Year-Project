9fc5f734b9e9bfc71a2c02f1813d739dc71695ac
Anders Wind Steffensen <awis@itu.dk>
2015-03-10 08:49:54 +0100

    ADded the activity model

---
+++ code/BNDN/WebAPI/Models/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public bool Executed { get; set; }
+        public bool Requested { get; set; }
+        public bool Excluded { get; set; }
+        public string Title { get; set; }
+    }
+}
