fe19300161af8e46167e3b46abb5a23f2d4ce3a5
Anders Fischer-Nielsen <andersfischern@me.com>
2015-03-10 09:06:22 +0100

    Moved Model classes.

---
+++ code/BNDN/Models/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public bool Executed { get; set; }
+        public bool Requested { get; set; }
+        public bool Excluded { get; set; }
+        public string Title { get; set; }
+        public Workflow ParentWorkflow { get; set; }
+        public int ParentId { get; set; }
+    }
+}
---
+++ code/BNDN/Models/Workflow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace Models
+{
+    public class Workflow
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Activity> Activities { get; set; }
+    }
+}
