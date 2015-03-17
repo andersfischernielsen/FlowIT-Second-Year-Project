53860652322aa6e7f721814bde750c5d2d198d4d
Anders Wind Steffensen <awia00@gmail.com>
2015-03-17 22:07:22 +0100

    Added some viewmodel code to the list view models

--- code/BNDN/Client/ViewModels/WorkflowListViewModel.cs
+++ code/BNDN/Client/ViewModels/WorkflowListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace Client.ViewModels
 {
     public class WorkflowListViewModel : ViewModelBase
     {
+        public WorkflowListViewModel()
+        {
+            WorkflowList = new ObservableCollection<WorkflowViewModel>();
+        }
+
+        #region Databindings
+
+        public ObservableCollection<WorkflowViewModel> WorkflowList { get; set; }
+
+        private WorkflowViewModel _selecteWorkflowViewModel;
+
+        public WorkflowViewModel SelectedWorkflowViewModel
+        {
+            get { return _selecteWorkflowViewModel; }
+            set
+            {
+                _selecteWorkflowViewModel = value;
+                NotifyPropertyChanged("SelectedWorkflowViewModel");
+            }
+        }
+
+        #endregion
+
+        #region Actions
+        #endregion
     }
 }
