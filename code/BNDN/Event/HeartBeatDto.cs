92df712e13e833c640c3d5388b8acb3688290c6b
Morten Albertsen <moalb@itu.dk>
2015-03-18 22:13:10 +0100

    Initial draft on Event-Server communication module

---
+++ code/BNDN/Event/HeartBeatDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Event
+{
+    class HeartBeatDto
+    {
+        public int EventId { get; set; }
+    }
+}
