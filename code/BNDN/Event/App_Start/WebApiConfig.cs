d979b1da2a3d6d687724bb10eb5d1a1eb058b863
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-03-22 12:18:15 +0100

    Chrome now defaults to JSON.

--- code/BNDN/Event/App_Start/WebApiConfig.cs
+++ code/BNDN/Event/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Event
@@ -19,6 +20,8 @@ namespace Event
                 routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
