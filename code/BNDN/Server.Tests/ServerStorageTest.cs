9f1187bdc79ad64242ba5a8b82b76fbae82d2c07
Morten Albertsen <moalb@itu.dk>
2015-03-23 22:25:51 +0100

    Added mostly empty unit-test project Server.tests with NUnit and Moq-libraries

---
+++ code/BNDN/Server.Tests/ServerStorageTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    public class ServerStorageTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            var dummy1 = new WorkflowDto() {Name = "Pay rent", Id = 1};
+            var dummy2 = new WorkflowDto() {Name = "How to get good grades", Id = 2};
+            var list = new List<WorkflowDto>() {dummy1, dummy2};
+
+            var mock = new Mock<IServerStorage>();
+            mock.Setup(x => x.GetAllWorkflows()).Returns(list);
+        }
+
+
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
