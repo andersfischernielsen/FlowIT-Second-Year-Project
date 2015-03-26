ce1852ecbbafb1da3423ce0adb7f0d9dea73276a
Anders Fischer-Nielsen <andersfischern@me.com>
2015-03-26 10:21:46 +0100

    Added interface for Event logic.

---
+++ code/BNDN/Event/Interfaces/IEventLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Event.Interfaces
+{
+    interface IEventLogic
+    {
+        Task UpdateRules(string id, EventRuleDto rules);
+
+        #region DTO Methods
+        Task<EventStateDto> EventStateDto { get; }
+        Task<EventDto> EventDto { get; }
+        #endregion
+    }
+}
