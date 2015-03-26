6e69a3568f32b9d045371f2037f3a9f865fc017e
Anders Fischer-Nielsen <andersfischern@me.com>
2015-03-26 10:22:14 +0100

    Refactoring of interface for Storing data about Events.

--- code/BNDN/Event/Interfaces/IEventStorage.cs
+++ code/BNDN/Event/Interfaces/IEventStorage.cs
@@ -8,40 +8,31 @@ namespace Event.Interfaces
 {
     public interface IEventStorage
     {
+        #region Ids
         Uri OwnUri { get; set; } //For notifying server about this event. Is fetched when receiving EventDto on creation!
         string WorkflowId { get; }
         string EventId { get; }
+        #endregion
 
-        #region Storage
+        #region State
         bool Executed { get; set; }
         bool Included { get; set; }
         bool Pending { get; set; }
         #endregion
 
         #region Rules
-        Task<HashSet<Uri>> Conditions { get; set; }
-        Task<HashSet<Uri>> Responses { get; set;  }
-        Task<HashSet<Uri>> Exclusions { get; set; }
-        Task<HashSet<Uri>> Inclusions { get; set; }
-
-        Task<IEnumerable<KeyValuePair<Uri, List<NotifyDto>>>> GetNotifyDtos();
-
-        Task UpdateRules(string id, EventRuleDto rules);
+        HashSet<Uri> Conditions { get; set; }
+        HashSet<Uri> Responses { get; set; }
+        HashSet<Uri> Exclusions { get; set; }
+        HashSet<Uri> Inclusions { get; set; }
         #endregion
 
-
-        #region DtoMethods
-        Task<EventStateDto> EventStateDto { get; }
-        Task<EventDto> EventDto { get; }
-        #endregion
-
-
-        #region EndPointRegistering
-        Task<Uri> GetUriFromId(string id);
-        Task<string> GetIdFromUri(Uri endPoint);
-        Task RegisterIdWithUri(string id, Uri endPoint);
-        Task<bool> KnowsId(string id);
-        Task RemoveIdAndUri(string id);
+        #region Id and Uri Handling
+        Uri GetUriFromId(string id);
+        string GetIdFromUri(Uri endPoint);
+        void RemoveIdAndUri(string id);
+        void StoreIdAndUri(string id, Uri endPoint);
+        bool IdExists(string id);
         #endregion
     }
 }
