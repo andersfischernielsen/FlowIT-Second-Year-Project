17ddc98bb535f4b4d32a6a95505629cc0a9cd3f1
Anders Wind Steffensen <awia00@gmail.com>
2015-03-29 17:22:29 +0200

    Added a test for the event Communicator

---
+++ code/BNDN/Event.tests/EventCommunicatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Event.Models;
+using NUnit.Framework;
+
+namespace Event.tests
+{
+    [TestFixture]
+    public class EventCommunicatorTests
+    {
+        [Test]
+        public void ConstructorRuns()
+        {
+            var eventCommunicator = new EventCommunicator(new Uri("http://test.dk/"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(HttpResponseException))]
+        public void IsExecutedFailsOnWrongUri()
+        {
+            var eventCommunicator = new EventCommunicator(new Uri("http://test.dk/"));
+            try
+            {
+                var result = eventCommunicator.IsExecuted().Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(HttpResponseException))]
+        public void IsIncludedFailsOnWrongUri()
+        {
+            var eventCommunicator = new EventCommunicator(new Uri("http://test.dk"));
+            try
+            {
+                var result = eventCommunicator.IsIncluded().Result;
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex.InnerException;
+            }
+
+        }
+
+    }
+}
