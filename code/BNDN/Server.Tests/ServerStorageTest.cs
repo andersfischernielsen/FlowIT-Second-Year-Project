ccf4bf06bcc4d5ac0ba61cc78ad07263df8a29c4
Anders Fischer-Nielsen <andersfischern@me.com>
2015-03-29 12:25:52 +0200

    Initial commit. Setup of Mock Storage.

--- code/BNDN/Server.Tests/ServerStorageTest.cs
+++ code/BNDN/Server.Tests/ServerStorageTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -16,12 +18,53 @@ namespace Server.Tests
         [SetUp]
         public void Setup()
         {
-            var dummy1 = new ServerWorkflowModel() {Name = "Pay rent", WorkflowId = "1"};
-            var dummy2 = new ServerWorkflowModel() { Name = "How to get good grades", WorkflowId = "2" };
-            var list = new List<ServerWorkflowModel>() { dummy1, dummy2 };
-
+            var w1 = new ServerWorkflowModel {Name = "w1", WorkflowId = "1"};
+            var w2 = new ServerWorkflowModel { Name = "w2", WorkflowId = "2" };
+            var list = new List<ServerWorkflowModel> { w1, w2 };
             var mock = new Mock<IServerStorage>();
-            mock.Setup(x => x.GetAllWorkflows()).Returns(list);
+
+            mock.Setup(m => m.AddEventToWorkflow(It.IsAny<ServerWorkflowModel>(), It.IsAny<ServerEventModel>()))
+                .Callback((ServerWorkflowModel workFlowToAddTo, ServerEventModel eventToAdd) => list.Find(e => e.Name == workFlowToAddTo.Name).ServerEventModels.Add(eventToAdd));
+
+            mock.Setup(m => m.AddNewWorkflow(It.IsAny<ServerWorkflowModel>()))
+                .Callback((ServerWorkflowModel toAdd) => list.Add(toAdd));
+
+            mock.Setup(m => m.GetAllWorkflows())
+                .Returns(list);
+
+            mock.Setup(m => m.GetEventsOnWorkflow(It.IsAny<ServerWorkflowModel>()))
+                .Returns((ServerWorkflowModel toGet) => toGet.ServerEventModels);
+
+            mock.Setup(m => m.GetWorkflow(It.IsAny<string>()))
+                .Returns((string workflowId) => list.Find(x => x.WorkflowId == (workflowId)));
+
+            mock.Setup(m => m.RemoveEventFromWorkflow(It.IsAny<ServerWorkflowModel>(), It.IsAny<string>()))
+                .Callback((ServerWorkflowModel toRemoveFrom, string eventId) =>
+                {
+                    var events = list.Find(x => x.WorkflowId == toRemoveFrom.WorkflowId).ServerEventModels;
+                    var toRemove = events.First(x => x.EventId == eventId);
+                    events.Remove(toRemove);
+                });
+            
+            mock.Setup(m => m.RemoveWorkflow(It.IsAny<ServerWorkflowModel>()))
+                .Callback((ServerWorkflowModel toRemove) => list.Remove(toRemove));
+
+            mock.Setup(m => m.UpdateEventOnWorkflow(It.IsAny<ServerWorkflowModel>(), It.IsAny<ServerEventModel>()))
+                .Callback((ServerWorkflowModel toUpdateOn, ServerEventModel eventToUpdate) =>
+                {
+                    var events = list.Find(x => x.WorkflowId == toUpdateOn.WorkflowId).ServerEventModels;
+                    var toRemove = events.First(x => x.EventId == eventToUpdate.EventId);
+                    var index = events.IndexOf(toRemove);
+                    events.Insert(index, eventToUpdate);
+                });
+
+            mock.Setup(m => m.UpdateWorkflow(It.IsAny<ServerWorkflowModel>()))
+                .Callback((ServerWorkflowModel toUpdate) =>
+                {
+                    var oldWorkflow = list.Find(x => x.WorkflowId == toUpdate.WorkflowId);
+                    var index = list.IndexOf(oldWorkflow);
+                    list.Insert(index, toUpdate);
+                });
         }
 
 
