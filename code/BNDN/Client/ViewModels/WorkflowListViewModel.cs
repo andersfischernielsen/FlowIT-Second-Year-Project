ba9600a4dc687437c980d8573316376e3f7fac1f
Anders Fischer-Nielsen <andersfischern@me.com>
2015-03-30 11:42:37 +0200

    Removal of dummy data on launch.

--- code/BNDN/Client/ViewModels/WorkflowListViewModel.cs
+++ code/BNDN/Client/ViewModels/WorkflowListViewModel.cs
@@ -10,40 +10,7 @@ namespace Client.ViewModels
         public WorkflowListViewModel()
         {
             WorkflowList = new ObservableCollection<WorkflowViewModel>();
-#if DEBUG
-            WorkflowList.Add(new WorkflowViewModel(new WorkflowDto())
-            {
-                Name = "Workflow1",
-                EventList = new ObservableCollection<EventViewModel>()
-            {
-                new EventViewModel(){Id = "ID11", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID12", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID13", Uri = new Uri("https://www.google.dk/")},
-            }
-            });
-            WorkflowList.Add(new WorkflowViewModel(new WorkflowDto())
-            {
-                Name = "Workflow2",
-                EventList = new ObservableCollection<EventViewModel>()
-            {
-                new EventViewModel(){Id = "ID21", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID22", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID23", Uri = new Uri("https://www.google.dk/")},
-            }
-            }); WorkflowList.Add(new WorkflowViewModel(new WorkflowDto())
-            {
-                Name = "Workflow3",
-                EventList = new ObservableCollection<EventViewModel>()
-            {
-                new EventViewModel(){Id = "ID31", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID32", Uri = new Uri("https://www.google.dk/")},
-                new EventViewModel(){Id = "ID33", Uri = new Uri("https://www.google.dk/")},
-            }
-            });
-            SelectedWorkflowViewModel = WorkflowList[0];
-#else
             GetWorkflows();
-#endif
         }
 
         #region Databindings
@@ -70,26 +37,14 @@ namespace Client.ViewModels
         {
             SelectedWorkflowViewModel = null;
             WorkflowList.Clear();
-            try
-            {
-                var connection = new ServerConnection(new Uri("http://localhost:13768/")); // todo get the real server address here
 
-                var test = await connection.GetWorkflows();
-                WorkflowList = new ObservableCollection<WorkflowViewModel>(test.Select(workflowDto => new WorkflowViewModel(workflowDto)));
-                if (WorkflowList.Count >= 1)
-                {
-                    SelectedWorkflowViewModel = WorkflowList[0];
-                }
-                else
-                {
-                    SelectedWorkflowViewModel = null;
-                }
-            }
-            catch (Exception)
-            {
-                //throw;
-            }
-            
+            //TODO: Implement exception handling.
+            var connection = new ServerConnection(new Uri("http://localhost:13768/")); // todo get the real server address here
+
+            var test = await connection.GetWorkflows();
+            WorkflowList = new ObservableCollection<WorkflowViewModel>(test.Select(workflowDto => new WorkflowViewModel(workflowDto)));
+            SelectedWorkflowViewModel = WorkflowList.Count >= 1 ? WorkflowList[0] : null;
+
             NotifyPropertyChanged("");
         }
 
