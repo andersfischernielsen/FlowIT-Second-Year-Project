19a650c6e7b983dd560336082f6e5080f67a2878
Anders Wind Steffensen <awis@itu.dk>
2015-03-30 14:40:25 +0200

    Added the two new Dtos for login from Client to Server

---
+++ code/BNDN/Common/RolesOnWorkflowsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class RolesOnWorkflowsDto
+    {
+        Dictionary<string, IList<string>> RolesOnWorkflows { get; set; }  // Key is workflowId, value is list of roles on that workflow.
+    }
+}
