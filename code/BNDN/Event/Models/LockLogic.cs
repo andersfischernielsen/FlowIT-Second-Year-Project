31c2f6e60cd662e380ee0667628740f51e14ba94
Morten Albertsen <kebabhans@hotmail.com>
2015-04-05 21:02:18 +0200

    Comments, null checks, and more descriptive variable-naming in LockLogic

--- code/BNDN/Event/Models/LockLogic.cs
+++ code/BNDN/Event/Models/LockLogic.cs
@@ -2,48 +2,69 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Common;
+using Event.Interfaces;
 
 namespace Event.Models
 {
+    /// <summary>
+    /// LockLogic is the class that handles the operations regarding locking this Event and other related, dependent Events. 
+    /// </summary>
     public class LockLogic
     {
         private readonly EventLogic _logic;
-        private readonly IEnumerable<Uri> _list;
-        private HashSet<Uri> _locked; 
+        /// <summary>
+        /// Because lock-logic is created for each request at a Controller, this _eventsToBelocked should not become outdated
+        /// TODO: Discuss: I am right about above, right?
+        /// </summary>
+        private readonly IEnumerable<Uri> _eventsToBelocked;
+        private HashSet<Uri> _lockedEvents; 
+        // TODO: MEGA-ÜBER non-chilled Morten: would an IEventLogic not be preferable / "more ideal" in constructor?
         public LockLogic(EventLogic logic)
         {
             _logic = logic;
-            _list = _logic.GetNotifyDtos().Result; // ER DET HER DEN RIGTIGE LISTE?
-            _locked = new HashSet<Uri>();
+            _eventsToBelocked = _logic.GetNotifyDtos().Result; // ER DET HER DEN RIGTIGE LISTE?
+            _lockedEvents = new HashSet<Uri>();
         }
 
-        // TODO: Will this method include locking this Event itself?
+        // TODO: Will this method include locking this Event itself? Or should caller take care of this it-self? 
         /// <summary>
         /// 
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A task containing a bool value indicating whether the operation of locking all related, dependent
+        /// events was successfull or not </returns>
         public async Task<bool> LockAll()
         {
-            if (_list == null)
+            if (_eventsToBelocked == null)
             {
-                throw new InvalidOperationException("_list is not instantiated");
+                throw new InvalidOperationException("_eventsToBelocked is not instantiated");
             }
+
+            // Attempt to lock down related, dependent Events down
             try
             {
+                // Initiate the lockDto that is to be passed to the other Events
+                // identifing this Event as the lockowner
                 LockDto lockDto = new LockDto {LockOwner = _logic.EventId};
+                
+                // Set this Event's own lockDto (so the Event know for the future that it locked itself down)
                 _logic.LockDto = lockDto;
 
-                foreach (var uri in _list)
+                // For every related, dependent Event, attempt to lock it
+                foreach (var uri in _eventsToBelocked)
                 {
                     await new EventCommunicator(uri).Lock(lockDto);
-                    _locked.Add(uri);
+                    // Discuss: Aren't we too naive here, just assuming that the Event *actually* DID lock itself? 
+                    // What if it failed to do so? We shouldn't add it to the _lockedEvents list then...
+                    // TODO: Read above! 
+                    _lockedEvents.Add(uri);
                 }
-                //TODO: Brug Parrallel i stedet for
-                //Parallel.ForEach(_list, async pair =>
-                //{
-                //    await new EventCommunicator(pair.Key).Lock(lockDto);
-                //    _locked.TryAdd(pair.Key, 0);
-                //});
+
+                //TODO: Brug Parrallel i stedet for:
+                // Parallel.ForEach(_eventsToBelocked, async uri =>
+                // {
+                //     await new EventCommunicator(uri).Lock(lockDto);
+                //     _lockedEvents.Add(uri);
+                // });
 
                 return true;
             }
@@ -58,7 +79,7 @@ namespace Event.Models
         {
             EventAddressDto eventAddress = new EventAddressDto() {Id = _logic.EventId, Uri = _logic.OwnUri};
 
-            var parallelTasks = Parallel.ForEach(_locked, pair =>
+            var parallelTasks = Parallel.ForEach(_lockedEvents, pair =>
             {
                 try
                 {
@@ -74,22 +95,27 @@ namespace Event.Models
             while (!parallelTasks.IsCompleted)
             {
             }
-            _locked = null;
+            _lockedEvents = null;
             _logic.LockDto = null;
         }
 
         public async Task<bool> UnlockAll()
         {
 
-            if (_list == null)
+            if (_eventsToBelocked == null)
             {
-                throw new InvalidOperationException("_list must not be null");
+                throw new InvalidOperationException("_eventsToBelocked must not be null");
             }
-            bool b = true;
 
+            // Optimistic approach; assuming every unlocking goes well, everyEventIsUnlocked will go unaffected 
+            bool everyEventIsUnlocked = true;
+
+            // Create the same identifier, that was used when locking the Events
             var eventAddress = new EventAddressDto() { Id = _logic.EventId, Uri = _logic.OwnUri };
 
-            foreach (var uri in _list)
+            // Unlock the other Events. 
+            // TODO: Do sazzy Parallel.ForEach here, too...?
+            foreach (var uri in _eventsToBelocked)
             {
                 try
                 {
@@ -98,12 +124,14 @@ namespace Event.Models
                 catch (Exception)
                 {
                     // TODO: Find out what to do if you cant even unlock. Even.
-                    b = false;
+                    everyEventIsUnlocked = false;
                 }
             }
 
+            // Finally, unlock this Event itself. 
             _logic.UnlockEvent();
-            return b;
+
+            return everyEventIsUnlocked;
         }
     }
 }
