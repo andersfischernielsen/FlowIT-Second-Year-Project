62b2ccf842319ca4863a212d462e4b49dd250e89
Morten Albertsen <kebabhans@hotmail.com>
2015-04-06 17:02:01 +0200

    Updated client to target Events at new routes

--- code/BNDN/Client/EventConnection.cs
+++ code/BNDN/Client/EventConnection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Common;
 
@@ -6,19 +7,20 @@ namespace Client
 {
     public class EventConnection : IEventConnection
     {
-        private readonly HttpClientToolbox _http;
+        private readonly HttpClientToolbox _httpClient;
         private readonly EventAddressDto _eventDto;
         public static Dictionary<string, IList<string>> RoleForWorkflow { get; set; }
 
         public EventConnection(EventAddressDto eventDto)
         {
-            this._eventDto = eventDto;
-            _http = new HttpClientToolbox(_eventDto.Uri);
+            _eventDto = eventDto;
+            _httpClient = new HttpClientToolbox(_eventDto.Uri);
         }
 
         public async Task<EventStateDto> GetState()
         {
-            return await _http.Read<EventStateDto>("event/state/-1");
+            var eventId = _eventDto.Id;
+            return await _httpClient.Read<EventStateDto>(String.Format("events/{0}/state/-1", eventId));   
         }
         
 
@@ -27,7 +29,8 @@ namespace Client
             IList<string> roles;
             RoleForWorkflow.TryGetValue(workflowId, out roles);
 
-            await _http.Update("event/executed", new ExecuteDto { Roles = roles });
+            var eventId = _eventDto.Id;
+            await _httpClient.Update(String.Format("events/{0}/executed/{1}",eventId,b),new ExecuteDto{Roles = roles});
         }
     }
 }
