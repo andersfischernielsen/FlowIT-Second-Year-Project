e1d0c02f65f83a51724a3184f4bfdb2cf4620bc6
Anders Wind Steffensen <awis@itu.dk>
2015-04-06 19:10:44 +0200

    Added a new model to get Rule Uri and ID

---
+++ code/BNDN/Event/Models/RelationToOtherEventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Event.Models
+{
+    /// <summary>
+    /// This class is not saved in the database.
+    /// </summary>
+    public class RelationToOtherEventModel
+    {
+        public string EventID { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
