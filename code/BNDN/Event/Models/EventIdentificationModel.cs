1d81f1e3bccb16d36e527e2f7b0a209ced83553c
Anders Wind Steffensen <awis@itu.dk>
2015-04-06 15:03:16 +0200

    Updated the EventIdentificationModel and EventStateModel such that they follow CodeFirst Standard as well as DB standards

--- code/BNDN/Event/Models/EventIdentificationModel.cs
+++ code/BNDN/Event/Models/EventIdentificationModel.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Event.Models.UriClasses;
 
 namespace Event.Models
 {
     public class EventIdentificationModel
     {
+        public EventIdentificationModel()
+        {
+            ResponseUris = new List<ResponseUri>();
+            InclusionUris = new List<InclusionUri>();
+            ExclusionUris = new List<ExclusionUri>();
+            ConditionUris = new List<ConditionUri>();
+        }
         [Key]
-        public int Id { get; set; }
+        public string Id { get; set; }
+
         public string OwnUri { get; set; }
         public string WorkflowId { get; set; }
-        public string EventId { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
+
+        [Required]
+        public virtual EventStateModel EventStateModel { get; set; }
+
+        public ICollection<ResponseUri> ResponseUris { get; set; }
+        public ICollection<InclusionUri> InclusionUris { get; set; }
+        public ICollection<ExclusionUri> ExclusionUris { get; set; }
+        public ICollection<ConditionUri> ConditionUris { get; set; }
     }
 }
