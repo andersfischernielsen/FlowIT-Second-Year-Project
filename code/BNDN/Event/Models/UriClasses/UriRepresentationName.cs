5e7bc87e37fea6c0405bda11bc2836c3285fbe2f
Anders Wind Steffensen <awis@itu.dk>
2015-04-06 15:02:44 +0200

    Renamede en klasse hvis namespace ikke matchede

---
+++ code/BNDN/Event/Models/UriClasses/UriRepresentationName.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Event.Models.UriClasses
+{
+    public class UriRepresentationBase
+    {
+        // This Id should be used for internal Entity Framework representation only
+        // TODO: Consider making property private
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UriString { get; set; }
+        [Required]
+        public string EventId { get; set; }
+
+
+
+        
+        public string EventIdentificationModelId { get; set; }
+        public virtual EventIdentificationModel EventIdentificationModel { get; set; }
+    }
+}
