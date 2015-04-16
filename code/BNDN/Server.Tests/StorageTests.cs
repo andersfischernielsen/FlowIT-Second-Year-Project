19217d0b53d1c6148c0396e6671a89be29c35b72
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-16 14:35:47 +0200

    Rename.

---
+++ code/BNDN/Server.Tests/StorageTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Server.Models;
+using Server.Storage;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    class StorageTests
+    {
+        
+    }
+}
