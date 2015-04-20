2b9ce510c6570875060c98e66c06fbf8f09dbd82
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-04-20 21:02:51 +0200

    Added modelbuilder-statements to be sure of what happens in EF.

--- code/BNDN/Event/Storage/EventContext.cs
+++ code/BNDN/Event/Storage/EventContext.cs
@@ -15,6 +15,26 @@ namespace Event.Storage
                 .HasMany(e => e.Roles)
                 .WithRequired(role => role.Event)
                 .HasForeignKey(role => new { role.WorkflowId, role.EventId });
+
+            modelBuilder.Entity<ConditionUri>()
+                .HasRequired(c => c.Event)
+                .WithMany(e => e.ConditionUris)
+                .HasForeignKey(c => new { c.WorkflowId, c.EventId });
+
+            modelBuilder.Entity<ResponseUri>()
+                .HasRequired(c => c.Event)
+                .WithMany(e => e.ResponseUris)
+                .HasForeignKey(c => new { c.WorkflowId, c.EventId });
+
+            modelBuilder.Entity<InclusionUri>()
+                .HasRequired(c => c.Event)
+                .WithMany(e => e.InclusionUris)
+                .HasForeignKey(c => new { c.WorkflowId, c.EventId });
+
+            modelBuilder.Entity<ExclusionUri>()
+                .HasRequired(c => c.Event)
+                .WithMany(e => e.ExclusionUris)
+                .HasForeignKey(c => new { c.WorkflowId, c.EventId });
         }
 
         public DbSet<EventModel> Events { get; set; }
@@ -22,6 +42,5 @@ namespace Event.Storage
         public DbSet<ResponseUri> Responses { get; set; }
         public DbSet<InclusionUri> Inclusions { get; set; }
         public DbSet<ExclusionUri> Exclusions { get; set; }
-        // LockDto has been extracted out of EventState as it would become a class within a class (and as such would need workaround)
     }
 }
