24afa6ad7db0268d9ff3e562929264abfbacf00d
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-04-20 16:11:37 +0200

    Fix event DB-model

--- code/BNDN/Event/Models/EventRoleModel.cs
+++ code/BNDN/Event/Models/EventRoleModel.cs
@@ -6,9 +6,9 @@ namespace Event.Models
     public class EventRoleModel
     {
         [Key, Column(Order = 0)]
-        public string EventId { get; set; }
-        [Key, Column(Order = 1)]
         public string WorkflowId { get; set; }
+        [Key, Column(Order = 1)]
+        public string EventId { get; set; }
         public EventModel Event { get; set; }
         [Key, Column(Order = 2)]
         public string Role { get; set; }
