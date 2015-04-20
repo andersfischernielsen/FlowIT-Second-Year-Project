73e2ba1778b6a741549839e7d40574f0222bb36e
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-04-20 21:03:41 +0200

    Don't override without calling base! ;)

--- code/BNDN/Event/Migrations/Configuration.cs
+++ code/BNDN/Event/Migrations/Configuration.cs
@@ -1,25 +1,14 @@
-using Event.Models;
-using Event.Models.UriClasses;
+using System.Data.Entity.Migrations;
 using Event.Storage;
 
 namespace Event.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
     internal sealed class Configuration : DbMigrationsConfiguration<EventContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
-        }
-
-        protected override void Seed(EventContext context)
-        {
-            
         }
     }
 }
