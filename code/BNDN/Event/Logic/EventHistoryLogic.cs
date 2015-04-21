1a7e31b81f92d0e0721f5e1c433964545dd49194
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-21 15:41:16 +0200

    Implemented logic for saving History.

---
+++ code/BNDN/Event/Logic/EventHistoryLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Common.History;
+using Event.Interfaces;
+
+namespace Event.Logic
+{
+    public class EventHistoryLogic : IEventHistoryLogic {
+        private readonly IEventStorage _storage;
+
+        public EventHistoryLogic(IEventStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public Task SaveHistory(HistoryModel toSave)
+        {
+            var asDto = new HistoryModel
+            {
+                EventId = toSave.EventId,
+                HttpRequestType = toSave.HttpRequestType,
+                Message = toSave.Message,
+                MethodCalledOnSender = toSave.MethodCalledOnSender,
+                WorkflowId = toSave.WorkflowId
+            };
+
+            return _storage.SaveHistory(asDto);
+        }
+
+        public async Task<IQueryable<HistoryModel>> GetHistoryForEvent(string workflowId, string eventId)
+        {
+            return await _storage.GetHistoryForEvent(workflowId, eventId);
+        }
+    }
+}
