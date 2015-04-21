4093de9e23c623397bd3752b4a07916a373855d2
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-04-21 15:48:53 +0200

    Client: ServerConnection: Support for password.

--- code/BNDN/Client/Connections/IServerConnection.cs
+++ code/BNDN/Client/Connections/IServerConnection.cs
@@ -7,7 +7,7 @@ namespace Client.Connections
 {
     public interface IServerConnection : IDisposable
     {
-        Task<RolesOnWorkflowsDto> Login(string username);
+        Task<RolesOnWorkflowsDto> Login(string username, string password);
         Task<IList<WorkflowDto>> GetWorkflows();
         Task<IList<EventAddressDto>> GetEventsFromWorkflow(WorkflowDto workflow);
     }
