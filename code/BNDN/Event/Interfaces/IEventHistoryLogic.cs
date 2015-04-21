f96005872125a0eef7d49d45b93f8faf6147d834
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-21 15:40:57 +0200

    Created interfaces for saving History.

---
+++ code/BNDN/Event/Interfaces/IEventHistoryLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.History;
+
+namespace Event.Interfaces
+{
+    public interface IEventHistoryLogic 
+    {
+        Task SaveHistory(HistoryModel toSave);
+
+        Task<IQueryable<HistoryModel>> GetHistoryForEvent(string workflowId, string eventId);
+    }
+}
