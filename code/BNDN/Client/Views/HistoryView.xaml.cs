bc3be66decf5c2b9d7bf9619b81e4f087e05c605
Mikael Lindemann Jepsen <mlin@itu.dk>
2015-04-23 13:50:02 +0200

    Set DataContext on Grid

--- code/BNDN/Client/Views/HistoryView.xaml.cs
+++ code/BNDN/Client/Views/HistoryView.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Client.ViewModels;
 
 namespace Client.Views
@@ -27,8 +15,8 @@ namespace Client.Views
 
         public HistoryView(HistoryListViewModel history)
         {
-            DataContext = history;
             InitializeComponent();
+            HistoryGrid.DataContext = history;
         }
     }
 }
