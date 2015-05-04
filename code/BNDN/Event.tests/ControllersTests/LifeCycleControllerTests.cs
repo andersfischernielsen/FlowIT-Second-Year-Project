80e2fc092c2302ab54e43cc5b011b8860e2c3b2c
Anders Fischer-Nielsen <andersfischern@me.com>
2015-05-04 10:12:16 +0200

    Kill me now, please. (Testing has been setup correctly).

--- code/BNDN/Event.tests/ControllersTests/LifeCycleControllerTests.cs
+++ code/BNDN/Event.tests/ControllersTests/LifeCycleControllerTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Common.DTO.Event;
 using Common.DTO.History;
+using Common.DTO.Shared;
 using Event.Controllers;
 using Event.Interfaces;
 using Event.Logic;
@@ -26,8 +27,8 @@ namespace Event.Tests.ControllersTests
         public void SetUp()
         {
             ResetLists();
-            var historyMock = new Mock<IEventHistoryLogic>();
-            var lifecycleMock = new Mock<ILifecycleLogic>();
+            var historyMock = new Mock<IEventHistoryLogic>(MockBehavior.Strict);
+            var lifecycleMock = new Mock<ILifecycleLogic>(MockBehavior.Strict);
 
             historyMock.Setup(l => l.GetHistoryForEvent(It.IsAny<string>(), It.IsAny<string>()))
                 .Returns((string wId, string eId) => Task.Run( () => 
@@ -66,39 +67,110 @@ namespace Event.Tests.ControllersTests
                     }
                 );
 
-            lifecycleMock.Setup(x => x.CreateEvent(It.IsAny<EventDto>(), It.IsAny<Uri>()))
+            lifecycleMock.Setup(m => m.CreateEvent(It.IsAny<EventDto>(), It.IsAny<Uri>()))
                 .Callback((EventDto dto, Uri uri) =>
                 {
-                    
+                    _eventTestList.Add(ConvertDtoToEventModel(dto, uri));
                 });
+
+            lifecycleMock.Setup(m => m.DeleteEvent(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns((string workflowId, string eventId) =>
+                {
+                    return Task.Run(() =>
+                    {
+                        var toRemove = _eventTestList.FirstOrDefault(e => e.WorkflowId == workflowId && e.Id == eventId);
+                        if (toRemove != null) _eventTestList.Remove(toRemove);
+                    });
+                });
+
+            lifecycleMock.Setup(m => m.GetEventDto(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns((string workflowId, string eventId) =>
+                {
+                    return Task.Run(() => _eventTestList.Where(e => e.Id == eventId && e.WorkflowId == workflowId)
+                        .Select(e => ConvertEventModelToDto(e, workflowId, eventId)).First());
+                });
+
+            lifecycleMock.Setup(m => m.ResetEvent(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.Run(() => true));
 
             _toTest = new LifecycleController(lifecycleMock.Object, historyMock.Object);
         }
 
-        private EventModel ConvertDtoToEventModel(EventDto dto)
+        private static EventModel ConvertDtoToEventModel(EventDto dto, Uri uri)
         {
-            var conditions = new List<ConditionUri>();
-            dto.Conditions.ToList().ForEach(x => conditions.Add(new ConditionUri { UriString = x.Uri.AbsolutePath }));
-
-            var exclusions = new List<ConditionUri>();
-            dto.Exclusions.ToList().ForEach(x => conditions.Add(new ConditionUri { UriString = x.Uri.AbsolutePath }));
-
-            //FUCK THIS BULLSHIT.
-
-            return new EventModel
+            var @event = new EventModel
             {
-                Executed = dto.Executed,
+                Id = dto.EventId,
                 WorkflowId = dto.WorkflowId,
                 Name = dto.Name,
-                Id = dto.EventId,
+                Roles = dto.Roles.Select(role => new EventRoleModel { WorkflowId = dto.WorkflowId, EventId = dto.EventId, Role = role }).ToList(),
+                OwnUri = uri.AbsoluteUri,
+                Executed = dto.Executed,
                 Included = dto.Included,
-                ConditionUris = conditions,
-                // ExclusionUris = exclusions,
-                // InclusionUris = dto.Inclusions,
-                // Roles = dto.Roles,
-                // ResponseUris = dto.Responses,
-                Pending = dto.Pending
+                Pending = dto.Pending,
+                ConditionUris = dto.Conditions.Select(condition => new ConditionUri { WorkflowId = dto.WorkflowId, EventId = dto.EventId, ForeignEventId = condition.Id, UriString = condition.Uri.AbsoluteUri }).ToList(),
+                ResponseUris = dto.Responses.Select(response => new ResponseUri { WorkflowId = dto.WorkflowId, EventId = dto.EventId, ForeignEventId = response.Id, UriString = response.Uri.AbsoluteUri }).ToList(),
+                InclusionUris = dto.Inclusions.Select(inclusion => new InclusionUri { WorkflowId = dto.WorkflowId, EventId = dto.EventId, ForeignEventId = inclusion.Id, UriString = inclusion.Uri.AbsoluteUri }).ToList(),
+                ExclusionUris = dto.Exclusions.Select(exclusion => new ExclusionUri { WorkflowId = dto.WorkflowId, EventId = dto.EventId, ForeignEventId = exclusion.Id, UriString = exclusion.Uri.AbsoluteUri }).ToList(),
+                LockOwner = null,
+                InitialExecuted = dto.Executed,
+                InitialIncluded = dto.Included,
+                InitialPending = dto.Pending
             };
+
+            return @event;
+        }
+
+        private static EventDto ConvertEventModelToDto(EventModel e, string workflowId, string eventId)
+        {
+            var toReturn = new EventDto
+            {
+                EventId = eventId,
+                WorkflowId = workflowId,
+                Name = e.Name,
+                Roles = e.Roles.Select(role => role.Role),
+                Pending = e.Pending,
+                Executed = e.Executed,
+                Included = e.Included,
+                Conditions =
+                    e.ConditionUris.Select(
+                        uri =>
+                            new EventAddressDto
+                            {
+                                WorkflowId = uri.WorkflowId,
+                                Id = uri.EventId,
+                                Uri = new Uri(uri.UriString)
+                            }),
+                Exclusions =
+                    e.ExclusionUris.Select(
+                        uri =>
+                            new EventAddressDto
+                            {
+                                WorkflowId = uri.WorkflowId,
+                                Id = uri.EventId,
+                                Uri = new Uri(uri.UriString)
+                            }),
+                Responses =
+                    e.ResponseUris.Select(
+                        uri =>
+                            new EventAddressDto
+                            {
+                                WorkflowId = uri.WorkflowId,
+                                Id = uri.EventId,
+                                Uri = new Uri(uri.UriString)
+                            }),
+                Inclusions =
+                    e.InclusionUris.Select(
+                        uri =>
+                            new EventAddressDto
+                            {
+                                WorkflowId = uri.WorkflowId,
+                                Id = uri.EventId,
+                                Uri = new Uri(uri.UriString)
+                            })
+            };
+
+            return toReturn;
         }
 
         [SetUp]
