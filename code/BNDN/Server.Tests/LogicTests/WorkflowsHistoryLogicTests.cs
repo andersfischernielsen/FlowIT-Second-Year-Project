50de8d5bca91f1910d3ea1b3686084859226f8d2
Anders Fischer-Nielsen <andersfischern@me.com>
2015-05-05 10:46:10 +0200

    Finished tests.

--- code/BNDN/Server.Tests/LogicTests/WorkflowsHistoryLogicTests.cs
+++ code/BNDN/Server.Tests/LogicTests/WorkflowsHistoryLogicTests.cs
@@ -35,6 +35,8 @@ namespace Server.Tests.LogicTests
                 .Returns((HistoryModel model) => 
                             Task.Run(() => _testModelList.Add(model))).Verifiable();
 
+            mock.Setup(m => m.Dispose()).Verifiable();
+
             _storageMock = mock;
             _toTest = new WorkflowHistoryLogic(mock.Object);
         }
@@ -46,10 +48,73 @@ namespace Server.Tests.LogicTests
         }
 
         [Test]
-        public void GetHistoryForWorkflowTest()
+        public async void GetHistoryForWorkflowTest()
         {
             //Setup.
-            var testHistory = new HistoryModel
+            var testHistory = CreateTestHistory();
+
+            _testModelList.Add(testHistory);
+
+            //Execute.
+            var collection = await _toTest.GetHistoryForWorkflow(@"&%¤#æøå*¨^´`?");
+            var result = collection.First();
+
+            //Assert.
+            _storageMock.Verify(m => m.GetHistoryForWorkflow(It.IsAny<string>()), Times.Once);
+            Assert.Throws<ArgumentNullException>(async () => await _toTest.GetHistoryForWorkflow(null));
+            Assert.DoesNotThrow(async () => await _toTest.GetHistoryForWorkflow(@"&%¤#æøå*¨^´`?"));
+            Assert.IsTrue(_testModelList.Any());
+            Assert.AreEqual(testHistory.WorkflowId, result.WorkflowId);
+            Assert.AreEqual(testHistory.EventId, result.EventId);
+            Assert.AreEqual(testHistory.HttpRequestType, result.HttpRequestType);
+            Assert.AreEqual(testHistory.Message, result.Message);
+            Assert.AreEqual("01/01/0001 00:00:00", result.TimeStamp);
+            Assert.AreEqual(testHistory.MethodCalledOnSender, result.MethodCalledOnSender);
+        }
+
+        [Test]
+        public void SaveHistoryTest()
+        {
+            //Setup.
+            var testHistory = CreateTestHistory();
+
+            //Execute.
+            Assert.DoesNotThrow(async () => await _toTest.SaveHistory(testHistory));
+
+            //Assert.
+            Assert.Throws<ArgumentNullException>(async () => await _toTest.SaveHistory(null));
+            _storageMock.Verify(m => m.SaveHistory(It.IsAny<HistoryModel>()), Times.Once);
+            Assert.IsTrue(_testModelList.Any());
+        }
+
+        public void SaveNoneWorkflowSpecificHistoryTest()
+        {
+            //Setup.
+            var testHistory = CreateTestHistory();
+
+            //Execute.
+            Assert.DoesNotThrow(async () => await _toTest.SaveNoneWorkflowSpecificHistory(testHistory));
+
+            //Assert.
+            Assert.Throws<ArgumentNullException>(async () => await _toTest.SaveNoneWorkflowSpecificHistory(null));
+            _storageMock.Verify(m => m.SaveNonWorkflowSpecificHistory(It.IsAny<HistoryModel>()), Times.Once);
+            Assert.IsTrue(_testModelList.Any());
+        }
+
+        [Test]
+        public void DisposeTest()
+        {
+            using (_toTest)
+            {
+                Assert.IsTrue(true);
+            }
+
+            _storageMock.Verify(m => m.Dispose(), Times.Once);
+        }
+
+        private static HistoryModel CreateTestHistory()
+        {
+            return new HistoryModel
             {
                 EventId = @"&%¤#æøå*¨^´`?",
                 WorkflowId = @"&%¤#æøå*¨^´`?",
@@ -58,30 +123,6 @@ namespace Server.Tests.LogicTests
                 MethodCalledOnSender = @"&%¤#æøå*¨^´`?",
                 TimeStamp = DateTime.MinValue
             };
-
-            _testModelList.Add(testHistory);
-
-            //Execute.
-            var result = _toTest.GetHistoryForWorkflow(@"&%¤#æøå*¨^´`?");
-
-            //Assert.
-            _storageMock.Verify(m => m.GetHistoryForWorkflow(It.IsAny<string>()), Times.Once);
-            Assert.IsTrue(_testModelList.Any());
-            Assert.AreEqual(testHistory, _testModelList.First());
-        }
-
-        public void SaveHistoryTest()
-        {
-
-        }
-
-        public void SaveNoneWorkflowSpecificHistoryTest()
-        {
-
-        }
-
-        public void DisposeTest()
-        {
         }
     }
 }
